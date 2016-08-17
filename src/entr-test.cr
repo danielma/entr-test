0646da719dace0aa3342f47d3fa6de0913e27bac
Daniel Ma <drailskid@yahoo.com>
2016-08-17 11:48:37 -0700

    it's a crytal elixir entr test runner

--- src/entr-test.cr
+++ src/entr-test.cr
@@ -1,5 +1,53 @@
-require "./entr-test/*"
+# this is our inspiration
 
-module Entr::Test
-  # TODO Put your code here
+# guard :elixir do
+#   watch(%r{^test/(.*)_test\.exs})
+#   watch(%r{^lib/(.+)\.ex$})           { |m| "test/#{m[1]}_test.exs" }
+#   watch(%r{^test/test_helper.exs$})   { "test" }
+# end
+
+base_command = "mix test"
+
+unless ARGV.size > 0
+  puts "Please pass at least one file as the first argument"
+  exit 1
 end
+
+$matches: Hash(Regex, Proc(Regex::MatchData, String) | String | Bool) = {
+  %r(^test/(.*)_test\.exs)   => true,
+  %r(^lib/(.+)\.ex$)         => ->(m: Regex::MatchData) { "test/lib/#{m[1]}_test.exs" },
+  %r(^web/(.+)\.ex$)         => ->(m: Regex::MatchData) { "test/#{m[1]}_test.exs" },
+  %r(^test/test_helper.exs$) => "",
+}
+
+def get_match(file)
+  $matches.each do |pattern, runner|
+    match = file.match(pattern)
+
+    return {file, runner, match} if match
+  end
+
+  nil
+end
+
+def try_match(file)
+  return nil unless match = get_match(file)
+
+  file = match[0]
+  runner = match[1]
+  match = match[2]
+
+  case runner
+  when Proc
+    runner.as(Proc).call(match)
+  when true
+    file
+  else
+    runner
+  end
+end
+
+test_file = try_match(ARGV[0])
+command = "mix test #{test_file} #{ARGV[1..-1].join(" ")}"
+puts command
+puts `#{command}`
--- src/entr-test/version.cr
+++
@@ -1,3 +0,0 @@
-module Entr::Test
-  VERSION = "0.1.0"
-end
